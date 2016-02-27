6e3b92274a5214b621df7f8780ba41b62806f383
smoll <mollah@gmail.com>
2016-02-27 00:33:43 -0500

    Use ActiveModelAdapter NOT JSONAPIAdapter

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,6 +1,6 @@
-import DS from 'ember-data';
+import ActiveModelAdapter from 'active-model-adapter';
 
-export default DS.JSONAPIAdapter.extend({
+export default ActiveModelAdapter.extend({
 	namespace: 'api/v1',
 	host: 'http://localhost:3000' // TODO: use env var to control this
 });
