42fdff83bb6924ba006cb515535401dd5e64508d
smoll <mollah@gmail.com>
2016-02-27 12:43:31 -0500

    Add contacts to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'smoll@example.com', password: 'takingcareofit', password_confirmation: 'takingcareofit')
+
+Contact.create!(
+  first_name: 'Jim',
+  last_name: 'Bob',
+  email: 'jim@example.com',
+  title: 'CEO of Acme, Inc.'
+)
+Contact.create!(
+  first_name: 'Satoshi',
+  last_name: 'Nakamoto',
+  email: 'satoshi@example.com',
+  title: 'CEO of Faker LLC'
+)
