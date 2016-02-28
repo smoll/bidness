c5e9c54f25c05ff99246ddc175b2cb403b9a251e
smoll <mollah@gmail.com>
2016-02-28 05:05:42 -0500

    Add bourbon and neat via bower & install bitters in 'public/assets'

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -4,7 +4,13 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      includePaths: [
+        'bower_components/bourbon/app/assets/stylesheets',
+        'bower_components/neat/app/assets/stylesheets',
+        'public/assets' // for bitters
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
